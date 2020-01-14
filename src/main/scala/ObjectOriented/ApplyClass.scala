2b71606c7af9d4748e327b7069421102c81180e8
lengo <chendaye666@gmail.com>
2020-01-14 08:52:15 +0800

    scala

---
+++ src/main/scala/ObjectOriented/ApplyClass.scala
@@ -0,0 +1,21 @@
+package ObjectOriented
+
+object ApplyClass {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
+
+
+/**
+ * 定义相互的
+ * */
+// object ApplyTest 的 伴生类
+class ApplyTest {
+
+}
+
+// class ApplyTest 的 伴生对象
+object ApplyTest {
+
+}
